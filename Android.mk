761cbbe0d07e37cdc979dd8a0d2e2fb3a20bc7f8
Chris Dearman <chris@mips.com>
2009-06-27 04:02:04 -0700

    [MIPS] Initial configuration
    Signed-off-by: Chris Dearman <chris@mips.com>

--- Android.mk
+++ Android.mk
@@ -1,3 +1,11 @@
+BUILD_OPROFILE:=false
 ifeq ($(TARGET_ARCH),arm)
+BUILD_OPROFILE:=true
+endif
+ifeq ($(TARGET_ARCH),mips)
+BUILD_OPROFILE:=true
+endif
+
+ifeq ($(BUILD_OPROFILE),true)
 include $(call all-subdir-makefiles)
 endif
